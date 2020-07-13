57981b56f5c5bce3c76908673dc014fe7be2b083
Msi Pc <Msi Pc@MSI>
2020-07-13 21:35:58 +0800

    updated test functions

--- shoppingcart/SpringBootH2DatabaseExampleApplicationTests2.java
+++ shoppingcart/SpringBootH2DatabaseExampleApplicationTests2.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -24,21 +25,32 @@ import static org.mockito.Mockito.*;
 //comment
 @SpringBootTest
 @ActiveProfiles("test")
-class SpringBootH2DatabaseExampleApplicationTests {
+class SpringBootH2DatabaseExampleApplicationTests2 {
 
 	@Autowired
 	SpringBootH2DatabaseExampleApplication SpringBootH2DatabaseExampleApplicationTests;
 
-	@MockBean
+	@Autowired
 	CartObjectRepository cor;
 
+	@Autowired
+	JmsMessagingTemplate jmsMessagingTemplate;
+
+	
 	@Test
-	public void run_test_save_return_null() {
+	public void run_test_save_return_not_null() {
 		//verify(shoppingItemSelectService, times(1)).simulateBatchItemSelect();
-		when(cor.save(any(CartObject.class)))
-        .thenReturn(null);
+		for (int i=0;i<5;i++) {
+			CartObject ca = new CartObject();
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			this.jmsMessagingTemplate.convertAndSend("CART_QUEUE.ACTIVE_QUEUE", ca);
+			System.out.println("Message has been put to queue by sender");
+		}
 	}
-	
-	
 
 }
