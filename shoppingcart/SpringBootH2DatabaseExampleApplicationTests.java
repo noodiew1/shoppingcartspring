1fe7c55d627eec7ef0857f52203645afc3c8adc4
Msi Pc <Msi Pc@MSI>
2020-07-13 10:15:50 +0800

    added comments

--- shoppingcart/SpringBootH2DatabaseExampleApplicationTests.java
+++ shoppingcart/SpringBootH2DatabaseExampleApplicationTests.java
@@ -5,27 +5,38 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import shoppingcart.cartobj.CartObject;
 import shoppingcart.cartobj.CartObjectRepository;
 import shoppingcart.message.MessageReceiverCartObject;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 
+@ContextConfiguration(classes = {
+		SpringBootH2DatabaseExampleApplication.class,
+	    CartObjectRepository.class
+	    
+})
+//comment
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringBootH2DatabaseExampleApplicationTests {
 
 	@Autowired
-	SpringBootH2DatabaseExampleApplication springBootH2DatabaseExampleApplication;
+	SpringBootH2DatabaseExampleApplication SpringBootH2DatabaseExampleApplicationTests;
 
-	//@MockBean
-	//ShoppingItemSelectService shoppingItemSelectService;
+	@MockBean
+	CartObjectRepository cor;
 
 	@Test
 	public void run_test_call() {
 		//verify(shoppingItemSelectService, times(1)).simulateBatchItemSelect();
+		when(cor.save(any(CartObject.class)))
+        .thenReturn(null);
 	}
 
 }
